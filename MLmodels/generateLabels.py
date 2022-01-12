c8154de2b68cc22b90e9959c8286c406de20dfa3
Brigt Haavardstun <brigt_arve@outlook.com>
2022-01-12 14:58:54 +0100

    making labels from bitmasks

---
+++ MLmodels/generateLabels.py
@@ -0,0 +1,37 @@
+
+
+from os import listdir
+from os.path import isfile, join
+
+def booleanFunction(A,B,C,D):
+    return (A or B) and (C or not D) #!!! MEGA IMPORTANT, defines ML
+
+def parseNameToLabel(fileName):
+    A = "A" in fileName 
+    B = "B" in fileName
+    C = "C" in fileName
+    D = "D" in fileName
+    return booleanFunction(A,B,C,D)
+
+# filename = A232, ABC19, BD23 etc.
+def generateLable(folderPrefix, filename):
+    storeValue= None
+    if parseNameToLabel(fileName=filename):
+        storeValue=1
+    else:
+        storeValue=0
+    
+    with open(f"{folderPrefix}{filename}.txt","w") as  f:
+        f.write(str(storeValue))
+
+
+def readAllFileNamesInTrainingData():
+    prefixFolder = "lables/"
+    directory = "training_data"
+    onlyfiles = sorted([f[:-4] for f in listdir(directory) if isfile(join(directory, f))])
+    for file in onlyfiles:
+        generateLable(prefixFolder,file)
+
+
+if __name__ == "__main__":
+    readAllFileNamesInTrainingData()
---
+++ MLmodels/lables/clean.sh
@@ -0,0 +1,3 @@
+for f in *.txt; do
+    rm $f
+done

e869eddc08ffb1b127f1e9ec394335667c428d09
Brigt Haavardstun <brigt_arve@outlook.com>
2022-02-05 21:31:33 +0100

    [RUN] Made the script work for the entire system

--- run.py
+++ run.py
@@ -1,3 +1,56 @@
-from CNN.run import run as go
+from CNN.trainCNN import run as generate_new_model
+from CNN.trainCNN import load_model
+from LM.lm import arg_min_ta
+from utils.dataset import load_dataset
+from PIL import Image
 
-go()
+import random
+
+
+
+def train_model():
+    generate_new_model(name="new model v1.0", save=True, verbose=True)
+
+def _sub_sample(valid_X, valid_Y, valid_labels, sample_size):
+    all_data_zip = []
+    for i in range(len(valid_labels)):
+        all_data_zip.append((valid_X[i], valid_Y[i], valid_labels[i]))
+
+
+    picks = random.sample(all_data_zip,sample_size)
+    
+    valid_X,valid_Y,valid_labels = zip(*picks)
+    return valid_X,valid_Y,valid_labels 
+
+def run_system():
+    #model, data = get_model(save=True,with_data_valid=True, verbose=True, name="StandarStrongModel")
+    #valid_X, valid_Y,valid_labels = data
+
+    #load datamodel
+    model= load_model(name="new model v1.0" )
+    
+    #get data TODO: should get less data
+    valid_X,valid_Y,valid_labels = load_dataset()
+    valid_X,valid_Y,valid_labels= _sub_sample(valid_X,valid_Y,valid_labels, 200)
+
+
+    one_test = valid_X[0]
+    print(model.predict(one_test))
+
+    pass
+    result = arg_min_ta(valid_X=valid_X, valid_Y=valid_Y, valid_labels=valid_labels, model_ai=model)
+
+
+    for (example_X, example_Y, example_label) in result:
+        Image.fromarray(example_X).show()
+        prediction = model.predict(example_X)
+        print(f"label: '{example_label}', Correct: {example_Y}, predicted: {prediction}")
+
+
+
+
+
+
+if __name__ == "__main__":
+    #run_system()
+    test()

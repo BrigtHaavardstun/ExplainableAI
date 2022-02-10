31dc11f708baebb0385200d29a6db09642747d5b
Brigt Haavardstun <brigt_arve@outlook.com>
2022-02-10 21:42:21 +0100

    [RUN] cRework run. Now modular for different deltas, lambda, beta etc.

--- run.py
+++ run.py
@@ -1,54 +1,99 @@
-from CNN.trainCNN import run as generate_new_model
-from CNN.trainCNN import load_model
-from LM.lm import arg_min_ta
-from utils.dataset import load_dataset
-from PIL import Image
+from models.trainModel import run as generate_new_model
+from models.trainModel import load_model
+from models.abstract_model import AbstractModel
+from models.CNN.CNNmodel import CNN
+from models.NN.NNmodel import NN
+
+from TA.TA import arg_min_ta
+
+from TA.subset.ISubset import ISubsetSelector
+from TA.subset.random_select import RandomSelect
+
+from TA.delta.IDelta import IDelta
+from TA.delta.sumOfLetters import SumOfLetters
+from TA.delta.maxLetter import MaxLetter
+from TA.delta.minLetter import MinLetter
+from TA.delta.squaredSum import SquaredSum
+
+from TA.Lambda.ILambda import ILambda
+from TA.Lambda.mean_square_error import MSE
+
+from utils.dataset import load_dataset, sub_sample
+from utils.save_data import save_data
+from utils.common import one_hot_to_number
+
 
 import random
+from PIL import Image
 
 
 
-def train_model():
-    generate_new_model(name="new model v1.0", save=True, verbose=True)
 
-def _sub_sample(valid_X, valid_Y, valid_labels, sample_size):
-    all_data_zip = []
-    for i in range(len(valid_labels)):
-        all_data_zip.append((valid_X[i], valid_Y[i], valid_labels[i]))
-
-
-    picks = random.sample(all_data_zip,sample_size)
+def train_model(model_to_train:AbstractModel=CNN, model_name:str="Defualt"):
+    generate_new_model(constructor= model_to_train, name=model_name, save=True, verbose=True)
     
-    valid_X,valid_Y,valid_labels = zip(*picks)
-    return valid_X,valid_Y,valid_labels 
 
-def run_system():
-    #model, data = get_model(save=True,with_data_valid=True, verbose=True, name="StandarStrongModel")
-    #valid_X, valid_Y,valid_labels = data
+def run_system(model:AbstractModel, set_selector:ISubsetSelector, delta:IDelta, compatibility_evalutator:ILambda,
+    valid_X,valid_Y,valid_labels,verbose=False, save=True, with_data_valid=False, name="Standar"):    
+    #get data TODO: This should be the same.
 
-    #load datamodel
-    model= load_model(name="new model v1.0" )
-    
-    #get data TODO: should get less data
-    valid_X,valid_Y,valid_labels = load_dataset()
-    valid_X,valid_Y,valid_labels= _sub_sample(valid_X,valid_Y,valid_labels, 200)
+   
 
 
-    result = arg_min_ta(valid_X=valid_X, valid_Y=valid_Y, valid_labels=valid_labels, model_ai=model)
+    picks, compatibility, complexity, boolexpr_best = arg_min_ta(verbose=verbose,valid_X=valid_X, valid_Y=valid_Y, valid_labels=valid_labels, ai_model=model,
+     set_selector=set_selector,
+      delta=delta, 
+      compatibility_evalutator=compatibility_evalutator
+    )
+    predictions = [str(one_hot_to_number(model.predict(x))) for x,y,label in picks]
+    #display_result(picks=picks, compatibility=compatibility, complexity=complexity, ai_model=model)
+    save_data(ai_model=model,bool_expr=boolexpr_best,picks=picks,predictions=predictions, compatibility=compatibility, complexity=complexity,
+        subset_selectors=set_selector, delta=delta, compatibility_evalutator=compatibility_evalutator)
 
-
-    for (example_X, example_Y, example_label) in result:
+def display_result(picks, compatibility, complexity, ai_model):
+    print(f"Overall score: {compatibility*100}, compatibility: {compatibility}, complexity: {complexity}")
+    for (example_X, example_Y, example_label) in picks:
         Image.fromarray(example_X).show()
-        prediction = model.predict(example_X)
+        prediction = ai_model.predict(example_X)
         print(f"label: '{example_label}', Correct: {example_Y}, predicted: {prediction}")
 
 
 
 
 
-import LM.QuineMcCluskey as QMC
-import LM.kMaps.Kamps as kMaps
 if __name__ == "__main__":
-    run_system()
-    #train_model()
-    #QMC.test()
+    model_name_CNN= "CNN v1.1"
+    train_model(model_to_train=CNN, model_name=model_name_CNN)
+
+    model_name_NN= "NN v1.1"
+    train_model(model_to_train=NN, model_name=model_name_NN)
+
+    subset_selectors = [RandomSelect()]
+    deltas = [SumOfLetters(), MaxLetter(), MinLetter(), SquaredSum()]
+    lambdas = [MSE()]
+
+    ai_models = [load_model(model_name_CNN),load_model(model_name_NN)]
+
+    valid_X,valid_Y,valid_labels =load_dataset()
+    valid_X,valid_Y,valid_labels= sub_sample(valid_X,valid_Y,valid_labels, 200)
+
+    
+    for subset_selector in subset_selectors:
+        for compatibility_evalutator in lambdas:
+            for delta in deltas:
+                for ai_model in ai_models:
+                    print(
+                        f"model: {ai_model}\n" +
+                        f"subset_selector: {subset_selector}\n"+
+                        f"compatibility_evalutator: {compatibility_evalutator}\n" +
+                        f"delta: {delta}"
+                        
+                    )
+                    run_system(model=ai_model,
+                    valid_X=valid_X,valid_Y=valid_Y,valid_labels=valid_labels,
+                    set_selector=subset_selector,
+                     delta=delta,
+                     compatibility_evalutator=compatibility_evalutator,
+                     verbose=True
+                     )
+

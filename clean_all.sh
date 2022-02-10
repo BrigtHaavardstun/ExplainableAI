5c9400c009042b722e77a74249662de98c217a1e
Brigt Haavardstun <brigt_arve@outlook.com>
2022-02-10 21:42:55 +0100

    [tools] clean all, generate new dataset.

---
+++ clean_all.sh
@@ -0,0 +1,10 @@
+cd data/images
+echo "Removing images..."
+rm *.png
+cd ../lables
+echo "Removing lables..."
+rm *.txt
+cd ../training_data
+echo "Removing training data..."
+rm *.bmp
+echo "Done!"

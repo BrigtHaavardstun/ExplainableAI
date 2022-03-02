23db995d7c1aab127a3c42d5472455a78d7ed950
Brigt Haavardstun <brigt_arve@outlook.com>
2022-03-02 18:12:34 +0100

    [run.py] change subset sample to be based on data_set_size

--- run.py
+++ run.py
@@ -21,93 +21,89 @@ from TA.Lambda.mean_square_error import MSE
 from utils.dataset import load_dataset, sub_sample
 from utils.save_data import save_data
 from utils.common import one_hot_to_number
-from utils.global_props import set_sample_attempts
+from utils.global_props import set_sample_attempts, DATA_SET_SIZE
 
 
 import random
 from PIL import Image
 
 
+def train_model(model_to_train: AbstractModel = CNN, model_name: str = "Defualt", verbose: bool = False):
+    generate_new_model(constructor=model_to_train,
+                       name=model_name, save=True, verbose=verbose)
 
 
-def train_model(model_to_train:AbstractModel=CNN, model_name:str="Defualt"):
-    generate_new_model(constructor= model_to_train, name=model_name, save=True, verbose=True)
-    
+def run_system(model: AbstractModel, set_selector: ISubsetSelector, delta: IDelta, compatibility_evalutator: ILambda,
+               valid_X, valid_Y, valid_labels, verbose=False, save=True, with_data_valid=False, name="Standar"):
+    # get data TODO: This should be the same.
 
-def run_system(model:AbstractModel, set_selector:ISubsetSelector, delta:IDelta, compatibility_evalutator:ILambda,
-    valid_X,valid_Y,valid_labels,verbose=False, save=True, with_data_valid=False, name="Standar"):    
-    #get data TODO: This should be the same.
-
-   
-
-
-    picks, compatibility, complexity, boolforest_best = arg_min_ta(verbose=verbose,valid_X=valid_X, valid_Y=valid_Y, valid_labels=valid_labels, ai_model=model,
-     set_selector=set_selector,
-      delta=delta, 
-      compatibility_evalutator=compatibility_evalutator
-    )
-    predictions = [str(one_hot_to_number(model.predict(x))) for x,y,label in picks]
+    picks, compatibility, complexity, boolforest_best = arg_min_ta(verbose=verbose, valid_X=valid_X, valid_Y=valid_Y, valid_labels=valid_labels, ai_model=model,
+                                                                   set_selector=set_selector,
+                                                                   delta=delta,
+                                                                   compatibility_evalutator=compatibility_evalutator
+                                                                   )
+    predictions = [str(one_hot_to_number(model.predict(x)))
+                   for x, y, label in picks]
     #display_result(picks=picks, compatibility=compatibility, complexity=complexity, ai_model=model)
-    save_data(ai_model=model,boolforest=boolforest_best,picks=picks,predictions=predictions, compatibility=compatibility, complexity=complexity,
-        subset_selectors=set_selector, delta=delta, compatibility_evalutator=compatibility_evalutator)
+    save_data(ai_model=model, boolforest=boolforest_best, picks=picks, predictions=predictions, compatibility=compatibility, complexity=complexity,
+              subset_selectors=set_selector, delta=delta, compatibility_evalutator=compatibility_evalutator)
+
 
 def display_result(picks, compatibility, complexity, ai_model):
-    print(f"Overall score: {compatibility*100}, compatibility: {compatibility}, complexity: {complexity}")
+    print(
+        f"Overall score: {compatibility*100}, compatibility: {compatibility}, complexity: {complexity}")
     for (example_X, example_Y, example_label) in picks:
         Image.fromarray(example_X).show()
         prediction = ai_model.predict(example_X)
-        print(f"label: '{example_label}', Correct: {example_Y}, predicted: {prediction}")
+        print(
+            f"label: '{example_label}', Correct: {example_Y}, predicted: {prediction}")
 
-def main_run_system(re_train = True):
-    model_name_CNN= "CNN v1.1"
+
+def main_run_system(re_train=True):
+    model_name_CNN = "CNN v1.1"
     if re_train:
         train_model(model_to_train=CNN, model_name=model_name_CNN)
 
-    model_name_NN= "NN v1.1"
+    model_name_NN = "NN v1.1"
     if re_train:
         train_model(model_to_train=NN, model_name=model_name_NN)
 
     subset_selectors = [RandomSelect()]
     deltas = [SumOfLetters(), MaxLetter(), MinLetter(), SquaredSum()]
     lambdas = [MSE()]
-    differentAttempts = [10,20,50,75,100,200,500,1000,2000]
-    ai_models = [load_model(model_name_CNN),load_model(model_name_NN)]
+    differentAttempts = [10, 20, 50, 75, 100, 200, 500, 1000, 2000]
+    ai_models = [load_model(model_name_CNN), load_model(model_name_NN)]
 
-    valid_X,valid_Y,valid_labels =load_dataset()
-    valid_X,valid_Y,valid_labels= sub_sample(valid_X,valid_Y,valid_labels, 200)
+    valid_X, valid_Y, valid_labels = load_dataset()
+    valid_X, valid_Y, valid_labels = sub_sample(
+        valid_X, valid_Y, valid_labels, len(valid_X)//2)
 
-    
     for subset_selector in subset_selectors:
         for compatibility_evalutator in lambdas:
             for delta in deltas:
                 for ai_model in ai_models:
                     for size in differentAttempts:
-                        #set global attempts to size.
+                        # set global attempts to size.
                         for i in range(3):
                             # we do three runs on each to get the average
                             # calulated afterwards
                             set_sample_attempts(size)
                             print(
                                 f"model: {ai_model}\n" +
-                                f"subset_selector: {subset_selector}\n"+
+                                f"subset_selector: {subset_selector}\n" +
                                 f"compatibility_evalutator: {compatibility_evalutator}\n" +
                                 f"delta: {delta}\n" +
                                 f"attemps: {size}"
-                                
+
                             )
                             run_system(model=ai_model,
-                            valid_X=valid_X,valid_Y=valid_Y,valid_labels=valid_labels,
-                            set_selector=subset_selector,
-                            delta=delta,
-                            compatibility_evalutator=compatibility_evalutator,
-                            verbose=True
-                            )
-
-
-
-
-
+                                       valid_X=valid_X, valid_Y=valid_Y, valid_labels=valid_labels,
+                                       set_selector=subset_selector,
+                                       delta=delta,
+                                       compatibility_evalutator=compatibility_evalutator,
+                                       verbose=True
+                                       )
 
 
 if __name__ == "__main__":
-    main_run_system(re_train=True)
+    main_run_system(re_train=False)

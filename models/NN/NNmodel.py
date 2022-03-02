06adbf3762a10ddee669b1764ba236b098d0fbda
Brigt Haavardstun <brigt_arve@outlook.com>
2022-03-02 18:05:26 +0100

    [FCNN] Add more layers to FCNN

--- models/NN/NNmodel.py
+++ models/NN/NNmodel.py
@@ -1,22 +1,16 @@
 import string
 import keras
 from keras.models import Input
-from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Dense, Dropout, Flatten, Activation
 from keras.layers.advanced_activations import LeakyReLU
 from models.abstract_model import AbstractModel
 from utils.global_props import IMAGE_WIDTH, IMAGE_HIGHT
-
-
-
-
+from keras.activations import sigmoid
 
 
 class NN(AbstractModel):
-    def __init__(self,name:str="Defualt",verbose:bool=True):
-        super(NN, self).__init__(name=name,verbose=verbose)
-
-        
-    
+    def __init__(self, name: str = "Defualt", verbose: bool = True):
+        super(NN, self).__init__(name=name, verbose=verbose)
 
     def _set_layers(self):
         """
@@ -25,19 +19,25 @@ class NN(AbstractModel):
         Activation function: LeakyReLU
         Pooling: MaxPool (2,2)
         """
-        input_width  = IMAGE_WIDTH
+        input_width = IMAGE_WIDTH
         input_height = IMAGE_HIGHT
         channels = 1
 
         num_classes = 2
         model = self.model
-        model.add(Input(shape=( input_width,input_height,channels))) 
+        model.add(Input(shape=(input_width, input_height, channels)))
         model.add(Flatten())
+        model.add(Dense(32, activation='linear'))
+        model.add(LeakyReLU(alpha=0.1))
+        model.add(Dropout(0.3))
+        model.add(Dense(16, activation='linear'))
+        model.add(LeakyReLU(alpha=0.1))
+        model.add(Dropout(0.3))
         model.add(Dense(8, activation='linear'))
         model.add(LeakyReLU(alpha=0.1))
-        #model.add(Dropout(0.3))
+        model.add(Dropout(0.3))
         model.add(Dense(4, activation='linear'))
-        model.add(LeakyReLU(alpha=0.1))            
-        #model.add(Dropout(0.3))
+        model.add(Activation(sigmoid))
+        model.add(Dropout(0.3))
         model.add(Dense(16, activation='linear'))
         model.add(Dense(num_classes, activation='softmax'))
